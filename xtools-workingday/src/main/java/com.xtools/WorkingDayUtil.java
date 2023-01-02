a3acdd4c798c79d0587e2457adeedd87b9a78a1a
zhuangjinxin <zhuangjinxin@hotmail.com>
2023-01-03 00:16:43 +0800

    feat: 增加工作日工具类的实现

--- xtools-workingday/src/main/java/com.xtools/WorkingDayUtil.java
+++ xtools-workingday/src/main/java/com.xtools/WorkingDayUtil.java
@@ -1,8 +1,8 @@
 package com.xtools;
 
-import java.time.Duration;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,20 +18,17 @@ public class WorkingDayUtil {
     }
 
     /**
-     * 节假日 Map
-     * Key： 日期
-     * Value 是否是节假日
+     * 节假日、调休日 等特殊日期的 Map
+     * Key： 日期 yyyy-MM-dd
+     * Value 是否是工作日
      */
-    private static final Map<String, Boolean> holidays = new HashMap<String, Boolean>();
+    private static final Map<String, Boolean> HOLIDAYS = new HashMap<>();
     /**
-     * 调休日 Map
-     * Key： 日期
-     * Value 是否是调休日
+     * 日期格式化 yyyy-MM-dd
      */
-    private static final Map<String, Boolean> workingDays = new HashMap<String, Boolean>();
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     static {
-
     }
 
     /**
@@ -42,7 +39,17 @@ public class WorkingDayUtil {
      * @return 结束日期
      */
     public static LocalDate getEndDate(LocalDate startDate, long days) {
-        return startDate.plusDays(days);
+        if (days < 0) {
+            throw new IllegalArgumentException("输入的工作日天数不合法");
+        }
+        LocalDate currentDate = startDate;
+        for (long i = days; i > 0; i--) {
+            currentDate = currentDate.plusDays(1);
+            if (!isWorkingDay(currentDate)) {
+                i = i + 1;
+            }
+        }
+        return currentDate;
     }
 
     /**
@@ -53,7 +60,17 @@ public class WorkingDayUtil {
      * @return 工作日天数
      */
     public static long getDays(LocalDate startDate, LocalDate endDate) {
-        return Duration.between(startDate, endDate).get(ChronoUnit.DAYS);
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("开始时间晚于结束时间，参数不合法。");
+        }
+
+        long days = 0;
+        for (LocalDate currentDate = startDate; currentDate.isBefore(endDate); currentDate = currentDate.plusDays(1)) {
+            if (isWorkingDay(currentDate)) {
+                days = days + 1;
+            }
+        }
+        return days;
     }
 
     /**
@@ -63,7 +80,13 @@ public class WorkingDayUtil {
      * @return 是不是工作日
      */
     public static boolean isWorkingDay(LocalDate date) {
-        return true;
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        String dateString = date.atStartOfDay().format(FORMATTER);
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            return HOLIDAYS.getOrDefault(dateString, false);
+        } else {
+            return HOLIDAYS.getOrDefault(dateString, true);
+        }
     }
 
     /**
@@ -72,6 +95,6 @@ public class WorkingDayUtil {
      * @return 今天是不是工作日
      */
     public static boolean isWorkingDay() {
-        return true;
+        return isWorkingDay(LocalDate.now());
     }
 }
